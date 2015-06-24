ddc9f6892fbe2144b532f8153eabffe61d77c3ff
Ryan Summe <ryan.summe@gmail.com>
2015-06-24 19:05:56 +0800

    add timer

--- Ribbit/ImageViewController.m
+++ Ribbit/ImageViewController.m
@@ -26,19 +26,11 @@
     self.navigationItem.title = [NSString stringWithFormat:@"Sent from %@", sender];
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (void)viewDidAppear:(BOOL)animated {
+    [super viewDidAppear:animated];
+    
+    [NSTimer scheduledTimerWithTimeInterval:10 target:self selector:@selector(timeout) userInfo:nil repeats:NO];
+    
 }
-
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
 
 @end
